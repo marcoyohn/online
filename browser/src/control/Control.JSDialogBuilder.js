45987c0d553073e696b3beede531917a77d6d295
杨茂聪 <yangmaocong@cvte.com>
2022-08-07 12:51:24 +0800

    fix: +

--- browser/src/control/Control.JSDialogBuilder.js
+++ browser/src/control/Control.JSDialogBuilder.js
@@ -1813,14 +1813,14 @@ L.Control.JSDialogBuilder = L.Control.extend({
 				}
 				if (windowZhFontsStr.indexOf(data[i]) != -1) {
 					var option = windowZhFontsOptions[data.entries[index]];
-					option.value = index+";" + option.value;
+					option.value = index + ';' + option.value;
 					if (isSelected) {
 						option.selected = true;
 						hasSelectedEntry = true;
 					}
 				} else {
 					var option = L.DomUtil.create('option', '', listbox);
-					option.value = index+";"+data.entries[index];
+					option.value = index+';'+data.entries[index];
 					option.innerText = data.entries[index];
 					if (isSelected) {
 						option.selected = true;
@@ -1833,7 +1833,7 @@ L.Control.JSDialogBuilder = L.Control.extend({
 		if (!hasSelectedEntry) {
 			if (title) {
 				var newOption = L.DomUtil.create('option', '', listbox);
-				newOption.value = ++index+";"+title;
+				newOption.value = ++index+';'+title;
 				newOption.innerText = title;
 				newOption.selected = true;
 			} else
