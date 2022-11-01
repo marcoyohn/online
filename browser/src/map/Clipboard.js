72dccc80b1fb55e5aafe5b10471556f31eee53b8
杨茂聪 <yangmaocong@cvte.com>
2022-11-01 11:38:14 +0800

    +

--- browser/src/map/Clipboard.js
+++ browser/src/map/Clipboard.js
@@ -663,8 +663,7 @@ L.Clipboard = L.Class.extend({
 						//});
 						L.Map.THIS._clip._doInternalPaste(L.Map.THIS, true);
 					}
-				})
-				.catch(function(err) {
+				}).catch(function(err) {
 					// 读取剪切板内容失败
 					console.error('Failed navigator.permissions.query: ', err);
 					L.Map.THIS._clip._doInternalPaste(L.Map.THIS, true);
